90d8b89acf6dfebc2b2b07139e114746558bb70f
Svetlik <skibochkareva@yandex.ru>
2024-11-08 21:26:02 +0400

    Добавить UserDefaults к счетчику

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -1,44 +1,44 @@
 import UIKit
 
+private let counterKey = "counter"
+
 final class ViewController: UIViewController {
     @IBOutlet private weak var label: UILabel!
     @IBOutlet private weak var textView: UITextView!
     
-    private(set) var counter: Int = 0
+    private(set) var counter: Int = UserDefaults.standard.integer(forKey: counterKey) {
+        didSet {
+            self.updateLabel()
+            UserDefaults.standard.set(counter, forKey: counterKey)
+        }
+    }
 }
 
 extension ViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        label.text = """
-            Значение счётчика:
-            0
-            """
-        textView.text = "История изменений:"
+        self.updateLabel()
     }
 }
 
 private extension ViewController {
     @IBAction func increaseCounter(_ sender: UIButton) {
         counter += 1
-        updateLabel()
         textView.text += prefix + "значение изменено на +1"
     }
     
     @IBAction func decreaseCounter(_ sender: Any) {
         if counter > 0 {
             counter = counter - 1
-            updateLabel()
             textView.text += prefix + "значение изменено на -1"
         } else {
             textView.text += prefix + "попытка уменьшить значение счётчика ниже 0"
         }
     }
     
-    @IBAction func zeroButton(_ sender: UIButton) {
+    @IBAction func zeroCounter(_ sender: UIButton) {
         counter = 0
-        updateLabel()
         textView.text += prefix + "значение сброшено"
     }
 
